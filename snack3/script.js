fcd6ba86f2f3274a78b3916920219cca55a1d596
GabrielePazi <pazigabriele@gmail.com>
2023-07-07 15:03:03 +0200

    1.2.1 modifiche indentazione html2 e script3

--- snack3/script.js
+++ snack3/script.js
@@ -3,10 +3,10 @@ const numberofArrays = parseInt(prompt("Inserisci un numero:"));
 for (let i = 0; i < numberofArrays; i++) {
   let randomArray = [];
   
-  for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
     let randomNumber = Math.ceil((Math.random() * 100) + 1)
     
     randomArray.push(randomNumber)
-    console.log(randomArray)
+    console.log(randomArray);
   }
 }
